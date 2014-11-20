e58c202fb386be28e6122614edadfce1100ceb8b
crazyyy <crazyyy@gmail.com>
2014-11-20 13:33:51 +0200

    walker end

--- functions.php
+++ functions.php
@@ -190,10 +190,85 @@ function wpeHeadNav()
 		'link_after'      => '',
 		'items_wrap'      => '<ul class="headnav">%3$s</ul>',
 		'depth'           => 0,
-		'walker'          => 'My_Walker'
+		'walker'          => new Description_Walker
 		)
 	);
 }
+/**
+ * Create HTML list of nav menu items.
+ * Replacement for the native Walker, using the description.
+ *
+ * @see    http://wordpress.stackexchange.com/q/14037/
+ * @author toscho, http://toscho.de
+ */
+class Description_Walker extends Walker_Nav_Menu
+{
+    /**
+     * Start the element output.
+     *
+     * @param  string $output Passed by reference. Used to append additional content.
+     * @param  object $item   Menu item data object.
+     * @param  int $depth     Depth of menu item. May be used for padding.
+     * @param  array $args    Additional strings.
+     * @return void
+     */
+    function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 )
+    {
+        $classes     = empty ( $item->classes ) ? array () : (array) $item->classes;
+
+        $class_names = join(
+            ' '
+        ,   apply_filters(
+                'nav_menu_css_class'
+            ,   array_filter( $classes ), $item
+            )
+        );
+
+        ! empty ( $class_names )
+            and $class_names = ' class="'. esc_attr( $class_names ) . '"';
+
+        $output .= "<li id='menu-item-$item->ID' $class_names>";
+
+        $attributes  = '';
+
+        ! empty( $item->attr_title )
+            and $attributes .= ' title="'  . esc_attr( $item->attr_title ) .'"';
+        ! empty( $item->target )
+            and $attributes .= ' target="' . esc_attr( $item->target     ) .'"';
+        ! empty( $item->xfn )
+            and $attributes .= ' rel="'    . esc_attr( $item->xfn        ) .'"';
+        ! empty( $item->url )
+            and $attributes .= ' href="'   . esc_attr( $item->url        ) .'"';
+
+        // insert description for top level elements only
+        // you may change this
+        $description = ( ! empty ( $item->description ) and 0 == $depth )
+            ? '<small class="nav_desc">' . esc_attr( $item->description ) . '</small>' : '';
+
+        $title = apply_filters( 'the_title', $item->title, $item->ID );
+
+        $item_output = $args->before
+            . "<a $attributes>"
+            . $args->link_before
+            . $title
+            . '</a> '
+            . $args->link_after
+            . $description
+            . $args->after;
+
+        // Since $output is called by reference we don't need to return anything.
+        $output .= apply_filters(
+            'walker_nav_menu_start_el'
+        ,   $item_output
+        ,   $item
+        ,   $depth
+        ,   $args
+        );
+    }
+}
+
+
+
 // WPE footer navigation
 function wpeFootNav() {
 	wp_nav_menu(
@@ -799,35 +874,4 @@ function top_level_cats_remove_cat_base($link)
 	return preg_replace('|' . $category_base . '|', '', $link, 1);
 }
 
-?>
-<?php
-    class My_Walker extends Walker_Nav_Menu
-{
-    function start_el(&$output, $item, $depth, $args) {
-        global $wp_query;
-        $indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
-
-        $class_names = $value = '';
-
-        $classes = empty( $item->classes ) ? array() : (array) $item->classes;
-
-        $class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item ) );
-        $class_names = ' class="' . esc_attr( $class_names ) . '"';
-
-        $output .= $indent . '<li id="menu-item-'. $item->ID . '"' . $value . $class_names .'>';
-
-        $attributes  = ! empty( $item->attr_title ) ? ' title="'  . esc_attr( $item->attr_title ) .'"' : '';
-        $attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target     ) .'"' : '';
-        $attributes .= ! empty( $item->xfn )        ? ' rel="'    . esc_attr( $item->xfn        ) .'"' : '';
-        $attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url        ) .'"' : '';
-
-        $item_output = $args->before;
-        $item_output .= '<a'. $attributes .'>';
-        $item_output .= '<span class="title">' . $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . '</span>' . $args->link_after;
-        $item_output .= '<span class="sub">' . $item->attr_title. '</span></a>'; /* This is where I changed things. */
-        $item_output .= $args->after;
-
-        $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
-    }
-}
 ?>
