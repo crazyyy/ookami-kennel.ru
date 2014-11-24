d4f5de6fb055c35c04d3ac9c52a6f454550a0b3c
Vitaly A <crazyyy@gmail.com>
2014-11-24 15:45:22 +0200

    cubrik rework

---
+++ wordpress/sxd/ses.php
@@ -0,0 +1,31 @@
+<?php
+$SES = array (
+  '4f4e2c9d3f9586b3d05a7e7fb9d3f857' => 
+  array (
+    'cfg' => 
+    array (
+      'charsets' => 'cp1251 utf8 latin1',
+      'lang' => 'uk',
+      'time_web' => '600',
+      'time_cron' => '600',
+      'backup_path' => 'backup/',
+      'backup_url' => 'backup/',
+      'only_create' => 'MRG_MyISAM MERGE HEAP MEMORY',
+      'globstat' => 0,
+      'my_host' => 'localhost',
+      'my_port' => 3306,
+      'my_user' => 'root',
+      'my_pass' => '',
+      'my_comp' => 0,
+      'my_db' => '',
+      'auth' => 'wp3 mysql cfg',
+      'user' => '',
+      'pass' => '',
+      'confirm' => '6',
+      'exitURL' => './',
+    ),
+    'time' => 1416335652,
+    'lng' => 'uk',
+  ),
+);
+?>
